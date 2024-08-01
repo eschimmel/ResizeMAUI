c47638783f982e81c1a070451b77b0dee21696b3
Ed Schimmel <easchimmel@outlook.com>
2024-08-01 10:20:21 +1000

    Restricting to landscape orientation

--- ResizeMAUI.Application.Maui/Platforms/Android/MainActivity.cs
+++ ResizeMAUI.Application.Maui/Platforms/Android/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace ResizeMAUI.Application.Maui
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,ScreenOrientation = ScreenOrientation.Landscape, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
--- ResizeMAUI.Models/Member.cs
+++ ResizeMAUI.Models/Member.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.Serialization;
-
-namespace ResizeMAUI.Models
+﻿namespace ResizeMAUI.Models
 {
     public partial class Member
     {
